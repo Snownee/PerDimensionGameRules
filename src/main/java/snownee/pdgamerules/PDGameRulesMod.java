ee32825ab942f66061c282c742e349477e364d26
Snownee <i@snownee.com>
2023-07-09 05:45:56 +0800

    1.20 port

--- src/main/java/snownee/pdgamerules/PDGameRulesMod.java
+++ src/main/java/snownee/pdgamerules/PDGameRulesMod.java
@@ -2,10 +2,10 @@ package snownee.pdgamerules;
 
 import java.util.Set;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 import com.google.common.collect.Sets;
+import com.mojang.logging.LogUtils;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
@@ -13,7 +13,7 @@ import net.minecraft.world.level.GameRules;
 
 public class PDGameRulesMod implements ModInitializer {
 
-	public static final Logger LOGGER = LogManager.getLogger("PDGameRules");
+	public static final Logger LOGGER = LogUtils.getLogger();
 	public static final Set<GameRules.Key<?>> UNSUPPORTED_GAME_RULES = Sets.newHashSet();
 	public static int generation = 0;
 
