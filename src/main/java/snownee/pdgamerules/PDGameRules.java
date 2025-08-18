8c3037034285d4f83d23a12b643cb90ee95bffd6
SettingDust <settingdust@gmail.com>
2025-08-18 12:49:42 +0800

    fix: crash when set gamerule

--- src/main/java/snownee/pdgamerules/PDGameRules.java
+++ src/main/java/snownee/pdgamerules/PDGameRules.java
@@ -7,6 +7,7 @@ import org.jetbrains.annotations.Nullable;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.collect.Maps;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
@@ -35,7 +36,7 @@ public class PDGameRules extends GameRules {
 	public <T extends Value<T>> @NotNull T getRule(Key<T> key) {
 		try {
 			return (T) cache.get(key, () -> {
-				Map<String, Object> rules = PDGameRulesConfig.rules.getOrDefault(dimension, Map.of());
+				Map<String, Object> rules = PDGameRulesConfig.rules.getOrDefault(dimension, Maps.newHashMap());
 				Object value = rules.get(key.getId());
 				if (value == null) {
 					return parent.getRule(key);
